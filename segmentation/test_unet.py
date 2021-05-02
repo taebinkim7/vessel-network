9f00bb8f8a671727c9e311b74169fea41048f6df
taebinkim <taebin.k.kim@gmail.com>
2021-05-02 16:26:08 -0400

    Update segmentation/test.py

--- segmentation/test_unet.py
+++ segmentation/test_unet.py
@@ -18,20 +18,20 @@ model.load_weights(latest_ckpt)
 
 # set paths for data
 test_data_dir = '../data/test_data'
+prediction_dir = glob(os.path.join(test_data_dir, 'predictions'))
+os.makedirs(prediction_dir, exist_ok=True)
 image_files = glob(os.path.join(test_data_dir, 'images/*'))
-mask_files = glob(os.path.join(test_data_dir, 'masks/*'))
 
 for image_file, mask_file in zip(image_files, mask_files):
+    file_name = os.path.basename(image_file)
 	image = np.array(Image.open(image_file))/255 # rescale the image
-	mask = np.array(Image.open(mask_file))
     old_size = image.shape[0:2]
     image_block_patches = make_block_patches(image, PATCH_SIZE)
-    mask_block_patches = make_block_patches(mask, PATCH_SIZE)
-    mask_block_coords = list(mask_block_patches.keys())
+    block_coords = list(image_block_patches.keys())
     test_patches = np.array(list(image_block_patches.values()))
     predicted_patches = model(test_patches, training=False)
     predicted_patches = adjust_prediction(predicted_patches)
-    predicted_patches = dict(zip(mask_block_coords, predicted_patches))
+    predicted_patches = dict(zip(block_coords, predicted_patches))
     predicted_mask = aggregate_block_patches(predicted_patches, old_size)
-    save_file = image_file[:-4] + '_prediction.png'
+    save_file = os.path.join(prediction_dir, file_name[:-4] + '_prediction.png')
     Image.fromarray(predicted_mask).save(save_file)
