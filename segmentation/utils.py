ec480735cde77c72780194ecc913a199889ac5ec
taebinkim <taebin.k.kim@gmail.com>
2021-05-03 00:24:21 -0400

    Fix error

--- segmentation/utils.py
+++ segmentation/utils.py
@@ -181,9 +181,6 @@ def aggregate_block_patches(block_patches, old_size=None):
     image = image
     if image.shape[2] == 1: # for 2-d arrays
         image = image.reshape(image.shape[0:2])
-    if np.max(image) <= 1: # to save the image using PIL.Image
-        image *= 255
-        image = image.astype(np.uint8)
 
     return image
 
